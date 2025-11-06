dde93fb6c9dae7b50c9b975a9ad8b980337ab768
Mario Schlipf <mario@Mac.localdomain>
2025-11-06 19:41:49 +0100

    Add logging

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import logging
 from .api import router as api_router
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-app.include_router(api_router)
 
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    logger.info(f"Request: {request.method} {request.url.path}")
+    response = await call_next(request)
+    return response
+
+
+app.include_router(api_router)
